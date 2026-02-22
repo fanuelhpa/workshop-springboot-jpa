18743dbd85f49fe4804ee8de5a19627096423cad
fanuelhpa <fanuelrique@gmail.com>
2026-02-21 21:15:31 -0300

    User entity and resource

---
+++ src/main/java/com/fandev/myfin/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.fandev.myfin.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fandev.myfin.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "99799999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
