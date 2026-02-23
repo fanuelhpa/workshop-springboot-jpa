03d44119aadf773d4eeb86717f340806fceebea5
fanuelhpa <fanuelrique@gmail.com>
2026-02-23 20:25:29 -0300

    Service layer, component registration

---
+++ src/main/java/com/fandev/myfin/services/UserService.java
@@ -0,0 +1,26 @@
+package com.fandev.myfin.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fandev.myfin.entities.User;
+import com.fandev.myfin.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.get();
+	}
+}
