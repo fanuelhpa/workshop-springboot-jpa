dfda24a272f5513ca78eaa91d2657b0faa3d1b3d
fanuelhpa <fanuelrique@gmail.com>
2026-02-25 23:08:22 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/com/fandev/myfin/entities/OrderItem.java
@@ -0,0 +1,87 @@
+package com.fandev.myfin.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.fandev.myfin.entities.pk.OrderItemPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private OrderItemPK id;
+	
+	private Integer quantity;
+	private Double price;
+	
+	public OrderItem() {
+		
+	}
+
+	public OrderItem(Order order, Product product,Integer quantity, Double price) {
+		super();
+		//O OrderItemPK é setado com order e product passados via construtor
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	//Para o mundo externo, tanto para post quanto para get, o que importa são order e product, 
+	//e não OrderItemPK, por isso os get e set serão de order e product
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
